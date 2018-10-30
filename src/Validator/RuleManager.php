272aa2a2944eb1d0e41adf5541ec645df90d2275
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2018-10-30 18:21:08 +0330

    Added Rule Functions

---
+++ src/Validator/RuleManager.php
@@ -0,0 +1,310 @@
+<?php
+
+
+namespace nimaebrazi\LaravelValidator\src\Validator;
+
+
+class RuleManager
+{
+
+    public function accepted()
+    {
+        return $this;
+    }
+
+    public function activeUrl()
+    {
+        return $this;
+    }
+
+    public function afterDate()
+    {
+        return $this;
+    }
+
+    public function afterOrEqualDate()
+    {
+        return $this;
+    }
+
+    public function alpha()
+    {
+        return $this;
+    }
+
+    public function alphaDash()
+    {
+        return $this;
+    }
+
+    public function alphaNumeric()
+    {
+        return $this;
+    }
+
+    public function array()
+    {
+        return $this;
+    }
+
+    public function bail()
+    {
+        return $this;
+    }
+
+    public function beforeDate()
+    {
+        return $this;
+    }
+
+    public function beforeOrEqualDate()
+    {
+        return $this;
+    }
+
+    public function between()
+    {
+        return $this;
+    }
+
+    public function boolean()
+    {
+        return $this;
+    }
+
+    public function confirmed()
+    {
+        return $this;
+    }
+
+    public function date()
+    {
+        return $this;
+    }
+
+    public function dateEquals()
+    {
+        return $this;
+    }
+
+    public function dateFormat()
+    {
+        return $this;
+    }
+
+    public function different()
+    {
+        return $this;
+    }
+
+    public function digits()
+    {
+        return $this;
+    }
+
+    public function digitsBetween()
+    {
+        return $this;
+    }
+
+    public function dimensionsImageFiles()
+    {
+        return $this;
+    }
+
+    public function distinct()
+    {
+        return $this;
+    }
+
+    public function email()
+    {
+        return $this;
+    }
+
+    public function existsDatabase()
+    {
+        return $this;
+    }
+
+    public function file()
+    {
+        return $this;
+    }
+
+    public function filled()
+    {
+        return $this;
+    }
+
+    public function greaterThan()
+    {
+        return $this;
+    }
+
+    public function greaterThanOrEqual()
+    {
+        return $this;
+    }
+
+    public function imageFile()
+    {
+        return $this;
+    }
+
+    public function in()
+    {
+        return $this;
+    }
+
+    public function inArray()
+    {
+        return $this;
+    }
+
+    public function integer()
+    {
+        return $this;
+    }
+
+    public function ipAddress()
+    {
+        return $this;
+    }
+
+    public function json()
+    {
+        return $this;
+    }
+
+    public function lessThan()
+    {
+        return $this;
+    }
+
+    public function lessThanOrEqual()
+    {
+        return $this;
+    }
+
+    public function max()
+    {
+        return $this;
+    }
+
+    public function mimeTypes()
+    {
+        return $this;
+    }
+
+    public function mimeTypeByFileExtension()
+    {
+        return $this;
+    }
+
+    public function min()
+    {
+        return $this;
+    }
+
+    public function notIn()
+    {
+        return $this;
+    }
+
+    public function notRegex()
+    {
+        return $this;
+    }
+
+    public function nullable()
+    {
+        return $this;
+    }
+
+    public function numeric()
+    {
+        return $this;
+    }
+
+    public function present()
+    {
+        return $this;
+    }
+
+    public function regularExpression()
+    {
+        return $this;
+    }
+
+    public function required()
+    {
+        return $this;
+    }
+
+    public function requiredIf()
+    {
+        return $this;
+    }
+
+    public function requiredUnless()
+    {
+        return $this;
+    }
+
+    public function requiredWith()
+    {
+        return $this;
+    }
+
+    public function requiredWithAll()
+    {
+        return $this;
+    }
+
+    public function requiredWithout()
+    {
+        return $this;
+    }
+
+    public function requiredWithoutAll()
+    {
+        return $this;
+    }
+
+    public function same()
+    {
+        return $this;
+    }
+
+    public function size()
+    {
+        return $this;
+    }
+
+    public function string()
+    {
+        return $this;
+    }
+
+    public function timezone()
+    {
+        return $this;
+    }
+
+    public function uniqueDatabase()
+    {
+        return $this;
+    }
+
+    public function url()
+    {
+        return $this;
+    }
+
+    public function uuid()
+    {
+        return $this;
+    }
+
+}
