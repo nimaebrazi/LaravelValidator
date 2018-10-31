3013d802f576d07a05aff3ee1f24f6dc218d4b50
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2018-10-31 21:00:09 +0330

    Added Test Libs

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace nimaebrazi\LaravelValidator\Test;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class ExampleTest extends OrchestraTestCase
+{
+    /** @test */
+    public function basic_test()
+    {
+        $this->assertTrue(true);
+    }
+}
